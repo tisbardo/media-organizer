1634fd5e1f64f39c1ba995f01f152f674517391d
Baptiste Lombardo <blombardo@ownpage.fr>
2020-04-05 23:53:41 +0200

    Added TvShow struct and name normalizing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use std::env;
 use std::fs;
 use std::ffi::OsString;
 
+struct TvShow {
+    normalized_name: String,
+    path: String,
+}
+
+impl TvShow {
+    pub fn new(path: OsString) -> TvShow {
+        let str_slice = path.to_str().unwrap();
+        TvShow {
+            path: String::from(str_slice),
+            normalized_name: normalize_show_name(str_slice)
+        }
+    }
+}
+
+struct File {
+    path: String,
+    
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -10,15 +30,25 @@ fn main() {
 
     println!("Start watching input directory {} and put into library {}", input_dir, library_dir);
 
-    let files = get_input_dir_files(input_dir);
+    let files = get_input_dir_files(input_dir.as_str());
+    let shows = get_series_in_library(library_dir.as_str());
 
-    for file in files {
-        println!("{}", file.to_str().unwrap())
-    }
+//    for file in files { println!("{}", file.to_str().unwrap()) }
+//    for show in shows { println!("{}", file.to_str().unwrap()) }
 }
 
-fn get_input_dir_files(input_dir: String) -> Vec<OsString> {
+fn get_input_dir_files(input_dir: &str) -> Vec<OsString> {
     return fs::read_dir(input_dir).expect("Cannot read input_dir").map(|file| {
         file.expect("cannot read file").file_name()
     }).collect()
+}
+
+fn get_series_in_library(library_dir: &str) -> Vec<TvShow> {
+    return fs::read_dir(library_dir).expect("Cannot read library_dir").map(|file| {
+        TvShow::new(file.expect("cannot read file").file_name())
+    }).collect()
+}
+
+fn normalize_show_name(name: &str) -> String {
+    name.replace(&['.', ' '][..], "")
 }
